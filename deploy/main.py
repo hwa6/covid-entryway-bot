b06dcfc972f240303dbb496b3e8a6880eea5ea18
Henry Anderson <handerson5486@gmail.com>
2022-04-20 14:47:51 -0400

    fixed indentation from vscode

--- deploy/main.py
+++ deploy/main.py
@@ -8,21 +8,21 @@ import display
 import model
 
 while(1):
-    if(true):
+    if(pir.detect):
         ##person detected, get label from model
         if(mask.detect() == False):
-            ##no mask, dispense mask
+            ##no mask, dispense masks
             display.displayStatus("No Mask Detected, Dispensing Mask.")
             mask.dispense()
-	    time.sleep(3)
+            time.sleep(3)
         else:
             ##mask on
             display.displayStatus("Mask Detected.")
-        time.sleep(1)
+            time.sleep(1)
         ##dispense hand sanitizer
         display.displayStatus("Dispensing Sanitizer")
         sanitizer.dispense()
         time.sleep(5)
     else:
-	#no one near sensor, wait a second before checking again
+        #no one near sensor, wait a second before checking again
         time.sleep(3)
